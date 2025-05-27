abe93a9b722e811901289f07ffe4a0a4e2cb1f3e
WoodenBrick1 <brickwooden@gmail.com>
2025-05-27 10:36:10 +0300

    Made buttons clickable

--- src/components/SongReviewer.jsx
+++ src/components/SongReviewer.jsx
@@ -1,11 +1,34 @@
 import Search from "./search"
 import TopBar from "./topBar"
-
+import {useState} from "react"
 import "../styles/SongReviewer.css"
 function SongReviewer(props) {
+  const [page, setPage] = useState("Search");
+
+  const renderPage = () => {
+    switch (page){
+      case "Search": 
+          return <Search />
+      case "Logout":
+          return <Logout />
+      case "Profile":
+          return <Profile />
+      case "Settings":
+          return <Settings />
+      case "Help":
+          return <Help />
+      case "About":
+          return <About />
+
+      default:
+          return <Search />
+    }
+  }
+
+  console.log(page);
     return (
         <>
-        <TopBar setDarkTheme={props.setDarkTheme} darkMode={props.darkMode}/>
+        <TopBar setDarkTheme={props.setDarkTheme} darkMode={props.darkMode} setPage={setPage}/>
         <main className ={`${props.darkMode ? "dark-mode" : ""}`}>
           <Search />
         </main>
--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -6,12 +6,12 @@ function TopBar(props) {
     <header className = {`${props.darkMode ? "dark-mode" : ""}`}>
         <h1 className="title">Song Reviewer</h1>
         <div className={`menu ${props.darkMode ? "dark-mode" : ""}`}>
-            <button>Search</button>
-            <button>Logout</button>
-            <button>Profile</button>
-            <button>Settings</button>
-            <button>Help</button>
-            <button>About</button>
+            <button onClick={() => {props.setPage("Search")}}>Search</button>
+            <button onClick={() => {props.setPage("Logout")}}>Logout</button>
+            <button onClick={() => {props.setPage("Profile")}}>Profile</button>
+            <button onClick={() => {props.setPage("Settings")}}>Settings</button>
+            <button onClick={() => {props.setPage("Help")}}>Help</button>
+            <button onClick={() => {props.setPage("About")}}>About</button>
         </div>
     </header>
     )
