db28863fc01ead01ffd3042050a28109c2327e7d
WoodenBrick1 <brickwooden@gmail.com>
2025-05-31 13:51:56 +0200

    Fixed Settings issues

--- src/components/Pages/Settings.jsx
+++ src/components/Pages/Settings.jsx
@@ -9,17 +9,21 @@ function Settings(props) {
         const newName = formData.get("name");
 
         props.setDarkMode(darkSetting ? true : false);
-        props.setUsername(newName);
+
+        if (newName == "")
+            return;
+        else
+            props.setUsername(newName);
     }   
     return (
     <form id="settings-container" onSubmit={submitSettings} className={`${props.darkMode ? "dark-mode" : ""}`}>
         <section>
             <label htmlFor="name">Username</label>
-            <input id="name" type="text" name="name"></input>
+            <input id="name" type="text" name="name" placeholder={props.username}></input>
         </section>
         <section>
             <label htmlFor="dark">Dark Mode</label>
-            <input id="dark"type="checkbox" name="darkMode" checked={props.darkMode}></input>
+            <input id="dark"type="checkbox" name="darkMode" defaultChecked={props.darkMode}></input>
         </section>
         <button type="submit" className="saveSettings">Save</button>
     </form>
